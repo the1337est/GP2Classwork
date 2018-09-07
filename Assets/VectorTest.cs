ceecb82fb5814bdff310d0078fa5563772e14638
Gaurav Tripathi <gauravtripathi198@gmail.com>
2018-09-07 11:44:30 -0400

    First commit

---
+++ Assets/VectorTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorTest : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+        Vector3 v1 = Vector3.one;
+        Vector3 zero = Vector3.zero;
+        Debug.Log("V1: " + v1 + "\nZero: " + zero);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
